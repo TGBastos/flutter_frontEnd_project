3cb40ec65bafd1eab4667c13dcf0ea5943258c89
Thiago Gomes <dm.thiagogb@hotmail.com>
2021-09-13 17:15:11 -0300

    Update loginButton.dart

---
+++ lib/Screens/widgets/loginButton.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class loginButton extends StatefulWidget {
+  const loginButton({ Key? key }) : super(key: key);
+
+  @override
+  _loginButtonState createState() => _loginButtonState();
+}
+
+class _loginButtonState extends State<loginButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
