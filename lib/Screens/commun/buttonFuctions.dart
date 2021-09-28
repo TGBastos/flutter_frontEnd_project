4cdcef7fd8e42a59c9837d492b96576e16ffbc2f
Thiago Gomes <dm.thiagogb@hotmail.com>
2021-09-28 18:16:05 -0300

    RioDasPedras

--- lib/Screens/commun/buttonFuctions.dart
+++ lib/Screens/commun/buttonFuctions.dart
@@ -1,11 +1,8 @@
-import 'dart:html';
-
-import 'package:rio_das_pedras_front_end/Screens/LoginScreens/widgets/loginForms.dart';
 import 'package:rio_das_pedras_front_end/core/entities/cpf.dart';
 
 class buttonFuctions {
   entrar(String ClientCPF) {
-    print(CPF(ClientCPF).formatedToString());
+    print(CPF(ClientCPF));
   }
 
   void cadastrar() {
