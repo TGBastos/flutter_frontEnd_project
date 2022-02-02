a1c60f4c44e1fce8eb16217a6927089e7a47c5a0
Dm-ThiagoGomes <dm.thiagogb@hotmail.com>
2022-02-02 16:24:40 -0300

    repositories

--- lib/View/cadastro/widgets/SigningUp_Path Steps/repositories.dart
+++ lib/View/cadastro/widgets/SigningUp_Path Steps/repositories.dart
@@ -1,10 +1,13 @@
-import 'package:rio_das_pedras_front_end/controllers/contatos_controller.dart';
+import 'package:rio_das_pedras_front_end/controllers/dados_pessoais_controller.dart';
 import 'package:rio_das_pedras_front_end/controllers/profissional_e_financeira_controller.dart';
 import 'package:rio_das_pedras_front_end/controllers/referencias_comerciais_controller.dart';
-import 'package:rio_das_pedras_front_end/controllers/referencias_pessoais_controller.dart';
-import 'package:rio_das_pedras_front_end/models/profissional_e_financeira_model.dart';
+
+import '../../../../controllers/contatos_controller.dart';
+import '../../../../controllers/referencias_pessoais_controller.dart';
 
 class Repositories {
+  static DadosPessoaisController dadosPessoaisController =
+      DadosPessoaisController();
   static ContatosController contatosRepositorie = ContatosController();
   static ProfissionalFinanceiraController profissionalFinanceiraRepositorie =
       ProfissionalFinanceiraController();
