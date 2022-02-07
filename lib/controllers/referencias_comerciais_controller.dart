9a25cc3815f94e8d5f2ccef987c13f3523850c9b
Dm-ThiagoGomes <dm.thiagogb@hotmail.com>
2022-02-07 11:01:33 -0300

    Update referencias_comerciais_controller.dart

--- lib/controllers/referencias_comerciais_controller.dart
+++ lib/controllers/referencias_comerciais_controller.dart
@@ -1,15 +1,19 @@
 import 'package:rio_das_pedras_front_end/models/referencias_comerciais_models.dart';
 
+import '../View/utils/remover_acentos.dart';
+
 class ReferenciasComerciaisController {
   ReferenciasComerciaisModel referenciaComercialModel =
       ReferenciasComerciaisModel();
 
   set referenciaComercialEmpresa1(String? valor) {
-    referenciaComercialModel.referenciaComercialEmpresa1 = valor ?? '';
+    valor = RemoverAcentos.removerAcentuacao(valor!);
+    referenciaComercialModel.referenciaComercialEmpresa1 = valor;
   }
 
   set referenciaComercialEmpresa2(String? valor) {
-    referenciaComercialModel.referenciaComercialEmpresa2 = valor ?? '';
+    valor = RemoverAcentos.removerAcentuacao(valor!);
+    referenciaComercialModel.referenciaComercialEmpresa2 = valor;
   }
 
   set referenciaComercialDDDTelefone1(String? valor) {
